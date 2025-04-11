90d12b3917ace1f80a9b62b9de7f3acf99c9eca5
John Morgan Wight <john.morgan.wight@proton.me>
2025-04-11 18:21:41 -0400

    Better print format

--- main.c
+++ main.c
@@ -86,6 +86,7 @@ int main(void)
 		if(isalpha(word[0]))
 			if((n = binsearch(word, keytab, NKEYS)) >= 0)
 				keytab[n].count++;
+	printf("\nKeywords Found:\n");
 	for(n = 0; n < NKEYS; n++)
 		if(keytab[n].count > 0)
 			printf("%4d %s\n", keytab[n].count, keytab[n].word);
